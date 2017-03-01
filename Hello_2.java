dc75654fa3dccbea85f068a4b473158bdcd0025a
manipy <mani.py@gmail.com>
2017-03-01 21:51:00 +0530

    new file:   Hello_2.java

---
+++ Hello_2.java
@@ -0,0 +1,9 @@
+public class Hello {
+	   public static void main(String[] args) {
+		         // Prints "Hello, World" in the terminal window.
+			  	  //test_1
+			  	  	  for(int i=1;i<=10;i++) {
+			 	  	        System.out.println("Hello, World"+i);
+			  	  	        		}
+			 	  	        		   }
+				  	        		   }
